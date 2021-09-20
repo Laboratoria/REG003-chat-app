66ac83330a7a0c075c47c3df9ed698b2dc0fffbe
mairelis-montilla <mairelismontilla@gmail.com>
2021-09-20 09:59:51 -0500

    ADD: midleware & auth route

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,10 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {runMiddleware, fn} from '../../../midlewares/auth'
 
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+    // Run the middleware
+    await runMiddleware(req, res, fn)
+      // Rest of the API logic
   if(req.method === 'GET'){
 
     res.status(200).json({ "name": req.method })
