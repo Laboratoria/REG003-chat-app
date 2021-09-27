77a06a7ec47410c66c1c8bc120ae9f7ecf2f70c7
Denisse Marinel Montalvo Castillo <marinel.casti@gmail.com>
2021-09-27 10:03:44 -0500

    fix:  file auth

--- controller/user.ts
+++ controller/user.ts
@@ -1,37 +1,42 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import prisma  from '../lib/prisma'
-import bcrypt from 'bcrypt';
-import { isValidEmail, isWeakPassword } from '../utils/utils';
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../lib/prisma";
+import bcrypt from "bcrypt";
+import { isValidEmail, isWeakPassword } from "../utils/utils";
 
-
-type CreateUserResponse = { id: number; email: string; password?: string; username: string,profile_image: string  }
+type CreateUserResponse = {
+  id: number;
+  email: string;
+  password?: string;
+  username: string;
+  profile_image: string;
+};
 //TODO-DONE format to email utils y password valid
-export const createUser = async (req: NextApiRequest, res: NextApiResponse) =>{
-  try{
+export const createUser = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
     const { username, email, password } = req.body;
-if(!username || !isValidEmail(email) || !isWeakPassword(password)){
-  console.log(username, email, password)
-  return res.status(400).json({ok:false, message:'bad request'})
-}
-const passwordCrypt = bcrypt.hashSync(password, 10);
-const newUser = await prisma.user.create({ data : { email, password:passwordCrypt, username } })
-const responseUser : CreateUserResponse= { ...newUser };
-console.log(newUser)
-delete responseUser.password;
-return res.json(responseUser)
-
-  }catch (error) {
-    if(error.code === 'P2002'){
-      console.log('error en prisma')
+    if (!username || !isValidEmail(email) || !isWeakPassword(password)) {
+      console.log(username, email, password);
+      return res.status(400).json({ ok: false, message: "bad request" });
+    }
+    const passwordCrypt = bcrypt.hashSync(password, 10);
+    const newUser = await prisma.user.create({
+      data: { email, password: passwordCrypt, username },
+    });
+    const responseUser: CreateUserResponse = { ...newUser };
+    console.log(newUser);
+    delete responseUser.password;
+    return res.json(responseUser);
+  } catch (error) {
+    if (error.code === "P2002") {
+      console.log("error en prisma");
       return res.status(400).json({
-        ok:false,
-        message: "Bad request"
-      })
+        ok: false,
+        message: "Bad request",
+      });
     }
     return res.status(500).json({
-      ok:false,
-      message: "server error"
-    })
+      ok: false,
+      message: "server error",
+    });
   }
-
-}
+};
--- services/auth.ts
+++ services/auth.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let url = "http://localhost:3000";
 //TODO: REFACTOR TO AWAIT / HANDLE ERRORS
 //ASSIGN TO: VALEN
@@ -9,3 +10,24 @@ export const postAuth = async (objValue: any) => fetch(`${url}/api/auth`, {
 ).then(response => response.json())
 .then(data => console.log(data))
 .catch(err=> console.error(err))
+=======
+let  url = "http://localhost:3000/api";
+
+export const postAuth = async (objValue:any) => {
+
+    console.log('LINE5', objValue.email);
+    console.log('LINE6', objValue.password);
+    
+    
+    const data = await fetch(`api/auth`,{
+        method:'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body:JSON.stringify(objValue)
+    })
+
+    console.log(data);
+    
+
+    return data 
+}
+>>>>>>> Stashed changes
