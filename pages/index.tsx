c3a178685f80e04136491f8a86979b530f64b7a9
mairelis-montilla <mairelismontilla@gmail.com>
2021-09-26 21:48:09 -0500

    Feat: Error message to register

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import { Space } from 'antd'
 import type { NextPage } from 'next'
-/* import Login from '../components/Login/Login' */
-import Register from '../components/Register/Register'
+import Login from '../components/Login/Login'
+/* import Register from '../components/Register/Register' */
 //TODO ROUTER
 const Home: NextPage = () => {
   return (
     <div className='container'>
-        <Register></Register>
+       {/*  <Register></Register> */}
+      <Login></Login>
     </div>
   )
 }
