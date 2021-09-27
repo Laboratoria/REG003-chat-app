4bc793d42216d9de9c577cb37402a9b6f4c60848
mairelis-montilla <mairelismontilla@gmail.com>
2021-09-26 21:47:49 -0500

    Feat: Error message to auth &  save token

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -8,17 +8,27 @@ import { postAuth } from '../../services/auth';
 
 const Login: NextPage = () => {
 
-    const onFinish = (values: any) => {
-        postAuth({email:values.email, password:values.password})
-      };
+  const [error, setError]= useState(false)
+
+  const onFinish = async (values: any) => {
+  const token:any = await postAuth(values)
+  console.log(token.ok)
+  if(!token.ok){
+    return setError(!token.ok)
+  }
+  else{
+    setError(!token.ok)
+    localStorage.setItem('token', token.token)
+//navegacion a otro lado
+  }
+}
       const onFinishFailed = (errorInfo: any) => {
-        console.log('Failed:', errorInfo);
+        console.log('Failed:');
       };
 
     return (
         <Form
       name="basic"
-      initialValues={{ remember: true }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
       autoComplete="off"
@@ -26,7 +36,7 @@ const Login: NextPage = () => {
       <Form.Item
         label="Email"
         name="email"
-        rules={[{ required: true, message: 'Please input your username!' }]}
+        rules={[{ required: true, message: 'Please input your Email!', pattern: /\S+@\S+\.\S+/, whitespace: true }]}
       >
         <Input />
       </Form.Item>
@@ -34,10 +44,13 @@ const Login: NextPage = () => {
       <Form.Item
         label="Password"
         name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
+        rules={[{required: true, message: 'Please input your password, min 6 and max 20!', whitespace: true, min: 6, max: 20 }]}
       >
         <Input.Password />
       </Form.Item>
+      {error ? <Form.Item>
+    <p>User or password incorret</p>
+  </Form.Item>: null}
 
       <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
         <Button type="primary" htmlType="submit">
