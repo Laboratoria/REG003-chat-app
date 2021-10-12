335d782e65a85a664197be49ceaacd9077523c6c
Briceyda <Brd0208@gmail.com>
2021-10-11 22:23:50 -0500

    Valid a email function

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,6 @@
-// module.exports = {
-
-// }
+module.exports = {
+  isAValidEmail: (email) => {
+    const emailRegex = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/i;
+    return emailRegex.test(email);
+  },
+};
