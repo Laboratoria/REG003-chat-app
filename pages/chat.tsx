1a802f6bd21bab721a8b2cb9147accbe8e66cd18
mairelis-montilla <mairelismontilla@gmail.com>
2021-10-12 20:51:23 -0500

    ADD: List chat component

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,12 +1,14 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
 import Header from '../components/Header/Header'
-import TabsMenu from "../components/Tabs/tabs";
+import TabsMenu from '../components/Tabs/tabs';
+import ListChat from '../components/List-Chat/ListChat'
 //TODO ROUTER
 const Chat: NextPage = () => {
   return (
-    <>
+    < >
       <Header></Header>
       <TabsMenu></TabsMenu>
+      <ListChat></ListChat>
     </>
   );
 };
