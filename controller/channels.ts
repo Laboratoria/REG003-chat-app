911d0d5fb99fb0ea45ab88ba40103e9d2e87897b
Valentina Zea <mavalezea@gmail.com>
2021-10-20 11:32:58 -0500

    Add: getAllChannels Endpoint

--- controller/channels.ts
+++ controller/channels.ts
@@ -1,3 +1,31 @@
-export const channels = () => {
-    console.log("Hola aquí channels");
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../lib/prisma";
+import err from '../middlewares/error';
+
+export const getAllChannels = async (req: NextApiRequest, res: NextApiResponse) => {
+
+    //TODO: IMPROVE QUERY GETALLUSER
+    try {
+        let myCursor = 1;
+        const allChannels = await prisma.channel.findMany({
+            take: 10,
+            skip: 1, // Skip the cursor
+            cursor: {
+                id: myCursor,
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        })
+        const lastPostInResults = allChannels[9] // Remember: zero-based index! :)
+        myCursor = lastPostInResults.id;
+
+        return res.status(200).json({
+            ok: true,
+            content: allChannels,
+            message: 'success'
+        })
+    } catch (error) {
+        return err(500, req, res);
+    }
 }
