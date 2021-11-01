b7f084b0c8009eaf2d885706739f9ab78f378dfc
mairelis-montilla <mairelismontilla@gmail.com>
2021-11-01 16:41:07 -0500

    modify components

--- components/Header_chat/HeaderChat.tsx
+++ components/Header_chat/HeaderChat.tsx
@@ -1,40 +1,48 @@
-import React from "react";
-import type { NextPage } from "next";
-import { PageHeader, Menu, Dropdown, Button } from "antd";
-import { MoreOutlined, SearchOutlined } from "@ant-design/icons";
-import { useRouter } from "next/router";
+import type { NextPage } from 'next';
+import { PageHeader, Menu, Dropdown, Button } from 'antd';
+import { MoreOutlined, } from '@ant-design/icons';
+import { useRouter } from 'next/router';
+import { deleteChannelUser } from '../../services/channelUser'
+import channelId from '../../pages/api/channel/[id]';
 
 
-{/* <Props> */}
-const HeaderChat: NextPage = () => {
-  const channelName='labo'
+interface Props {
+  token: any,
+  uid: number,
+  channelId: number
+
+}
+const HeaderChat: NextPage<Props> = ({token, uid, channelId}) => {
+  const channelName = 'labo'
   const router = useRouter();
+  const goOut = ()=>{
+    const channel= deleteChannelUser(token, uid, channelId)
+    router.push('/chat')
+    return channel
+  }
 
   const menu = (
     <Menu>
-      <Menu.Item key="settings">
+      <Menu.Item key='settings'>
         <p>Ajustes</p>
       </Menu.Item>
-      <Menu.Item key="showUsers">
-        <p>Ver Usuarios</p>
-      </Menu.Item>
-      <Menu.Item key="getOut"  >
+      <Menu.Item key='getOut' onClick={goOut} >
         <p> Salir del Chat</p>
       </Menu.Item>
     </Menu>
   );
   const DropdownMenu = () => (
-    <Dropdown key="more" overlay={menu}>
+    <Dropdown key='more' overlay={menu}>
       <Button
         style={{
-          border: "none",
+          border: 'none',
           padding: 0,
         }}
       >
         <MoreOutlined
           style={{
             fontSize: 25,
-            verticalAlign: "top",
+            verticalAlign: 'top',
           }}
         />
       </Button>
@@ -44,10 +52,10 @@ const HeaderChat: NextPage = () => {
     <>
       <PageHeader
         title={channelName}
-        onBack={() =>   router.push('/chat')}
-        className="site-page-header"
+        onBack={() => router.push('/chat')}
+        className='site-page-header'
         extra={[
-          <DropdownMenu key="more" />,
+          <DropdownMenu key='more' />,
         ]}
         avatar={{ src: 'https://avatars1.githubusercontent.com/u/8186664?s=460&v=4' }}
       ></PageHeader>
--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -33,14 +33,17 @@ const Chat: NextPage = () => {
     });
   }, []);
 
+  const token = localStorage.getItem("token");
+  let uid: any;
+  if (token) {
+    const payload = token.split(".")[1];
+    const decodedPayload = window.atob(payload);
+    const payloadJSON = JSON.parse(decodedPayload);
+    uid = payloadJSON.uid;
+  }
   useEffect(() => {
     //TODO PETICION A LA BD DE CANALES DE USUARIo
-    const token = localStorage.getItem("token");
     if (token) {
-      const payload = token.split(".")[1];
-      const decodedPayload = window.atob(payload);
-      const payloadJSON = JSON.parse(decodedPayload);
-      const uid = payloadJSON.uid;
       // console.log(payloadJSON.uid)
 
       getUserChannels(token, uid).then((res) => {
@@ -106,7 +109,9 @@ const Chat: NextPage = () => {
                 channelTitle={name}
                 description={description}
                 channelImage={channelImage}
-                id={id}
+                channelId={id}
+                token={token}
+                userId={uid}
               ></ListDiscover>
             </>
           );
